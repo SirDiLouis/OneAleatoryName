955cac4205ebf583219a1e19b29b2f9b9a81c6bf
SirDiLouis <diogoclasher4@gmail.com>
2020-08-10 14:05:36 -0300

    commit message

--- lib/main.dart
+++ lib/main.dart
@@ -1,139 +1,125 @@
-import 'package:flutter/cupertino.dart';
+import 'package:bubble_bottom_bar/bubble_bottom_bar.dart';
 import 'package:flutter/material.dart';
- 
-void main() {
-  runApp(MyApp());
-}
- 
+
+void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Remember the Mask',
+      title: 'Olha a Máscara!',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
- 
+
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
- 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
- 
-class _MyHomePageState extends State<MyHomePage> {
- 
+
+class _MyHomePageState extends State {
+  int currentIndex;
+
+  @override
+  void initState() {
+    super.initState();
+
+    currentIndex = 0;
+  }
+
+  changePage(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
- 
     return Scaffold(
+      backgroundColor: Colors.grey[100],
       appBar: AppBar(
-        actions: <Widget>[
-          IconButton(
-            alignment: Alignment.centerLeft,
-            icon: Icon(Icons.home), 
-            onPressed: null)
-        ]
+        title: Text('Página Inicial'),
+        centerTitle: true,
       ),
-      body: Stack(
-        children: <Widget>[
-          Container(
-            color: Color.fromRGBO(208, 208, 208, 0.5),
-            child: Column(
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    Container(height: 1, width: 1,),
-                    Padding(
-                      padding: const EdgeInsets.only(right: 30,top: 50),
-                    )
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 30, bottom: 18),
-                  child: Text('Hora de trocar a máscara!!!', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),),
-                ),
-                Text('Ter 04/08/2020', style: TextStyle(fontSize: 18, color: Colors.black54),),
-                Padding(
-                  padding: const EdgeInsets.only(left: 19,right: 19),
-                  child: Container(
-                    height: 100,
-                    child: Padding(
-                      padding: const EdgeInsets.all(19.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                            Text('Brasil', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
-                            Text('Dois Vizinhos', style: TextStyle(fontSize: 20, color: Colors.black54),)
-                          ],),
-                          Text('8:00', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),)
-                        ],
-                      ),
-                    ),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.all(Radius.circular(20))
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 19,right: 19, top: 13),
-                  child: Container(
-                    height: 100,
-                    child: Padding(
-                      padding: const EdgeInsets.all(19.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text('India', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
-                              Text('Pune', style: TextStyle(fontSize: 20, color: Colors.black54),)
-                            ],),
-                          Text('11:00', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),)
-                        ],
-                      ),
-                    ),
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.all(Radius.circular(20))
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ),
-          Positioned(
-            right: 1, bottom: 1,
-            child: Padding(
-              padding: const EdgeInsets.all(29.0),
-              child: Container(
-                height: 60, width: 60,
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                  borderRadius: BorderRadius.all(Radius.circular(24)),
-                  gradient: LinearGradient(colors: [Colors.blue, Color.fromRGBO(0, 41, 102, 1)])
-                ),
-                child: Icon(Icons.add, color: Colors.white60,),
+      bottomNavigationBar: BubbleBottomBar(
+        hasNotch: true,
+        fabLocation: BubbleBottomBarFabLocation.end,
+        opacity: .2,
+        currentIndex: currentIndex,
+        onTap: changePage,
+        borderRadius: BorderRadius.vertical(
+            top: Radius.circular(
+                16)),
+        elevation: 8,
+        items: <BubbleBottomBarItem>[
+          BubbleBottomBarItem(
+              backgroundColor: Colors.red,
+              icon: Icon(
+                Icons.dashboard,
+                color: Colors.black,
               ),
-            ),
-          )
+              activeIcon: Icon(
+                Icons.dashboard,
+                color: Colors.red,
+              ),
+              title: Text("Home")),
+          BubbleBottomBarItem(
+              backgroundColor: Colors.deepPurple,
+              icon: Icon(
+                Icons.access_time,
+                color: Colors.black,
+              ),
+              activeIcon: Icon(
+                Icons.access_time,
+                color: Colors.deepPurple,
+              ),
+              title: Text("Logs")),
+          BubbleBottomBarItem(
+              backgroundColor: Colors.indigo,
+              icon: Icon(
+                Icons.folder_open,
+                color: Colors.black,
+              ),
+              activeIcon: Icon(
+                Icons.folder_open,
+                color: Colors.indigo,
+              ),
+              title: Text("Folders")),
+          BubbleBottomBarItem(
+              backgroundColor: Colors.green,
+              icon: Icon(
+                Icons.menu,
+                color: Colors.black,
+              ),
+              activeIcon: Icon(
+                Icons.menu,
+                color: Colors.green,
+              ),
+              title: Text("Menu"))
         ],
-      )
+      ),
+      body: (currentIndex == 0)
+          ? Icon(
+              Icons.dashboard,
+              size: 150.0,
+              color: Colors.red,
+            )
+          : (currentIndex == 1)
+          ? Icon(
+              Icons.folder_open,
+              size: 150.0,
+              color: Colors.indigo,
+            )
+          : Icon(
+              Icons.assignment,
+                size: 150.0,
+                color: Colors.deepPurple,
+            ),
     );
   }
 }
---
+++ lib/screens/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MyStatefulWidget extends StatefulWidget {
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+@override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Página Inicial')
+      ),
+    );
+  }
+}
